9cf5b110033b99e4e3bf99053ae9b8ebed5dd43b
Daniel.Prchlik <Daniel.Prchlik@VT3-24>
2023-05-12 11:34:34 +0200

    Přidejte soubory projektu.

---
+++ Projekt Květen 2O23/Inventory.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Květen_2O23
+{
+    internal class Inventory
+    {
+        public List<Product> products = new List<Product>();
+        public void AddProduct()
+        {
+            Product product = new Product();
+            try
+            {
+                Console.Write("Napiš jméno: ");
+                product.name = Console.ReadLine();
+                Console.Write("Napiš cenu: ");
+                product.price = double.Parse(Console.ReadLine());
+                Console.Write("Napiš počet: ");
+                product.quantity = int.Parse(Console.ReadLine());
+                products.Add(product);
+                Console.WriteLine(product.name + " byl přidán");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Chyba: "+ ex.Message);
+            }
+        }
+        public void RemoveProduct()
+        {
+            Product product = new Product();
+            Console.WriteLine("Inventář: ");
+            foreach (Product produc in products)
+            {
+                Console.WriteLine(products.IndexOf(produc) + 1 + ")  " + produc.name + " - " + produc.quantity + "ks - " + produc.price + "Kč");
+            }
+            try
+            {
+                Console.WriteLine("------------------------");
+                Console.Write("Napiš pořadí produktu který bude odebrán: ");
+                int indexListu = int.Parse(Console.ReadLine()) -1;
+                products.RemoveAt(indexListu);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Chyba: " + ex.Message);
+            }
+        }
+        public void ShowInventory()
+        {
+            Console.WriteLine("Inventář: ");
+            foreach (Product product in products)
+            {
+                Console.WriteLine(products.IndexOf(product)+ 1 + ")  " + product.name +" - "+product.quantity+"ks - "+product.price+"Kč");
+            }
+        }
+    }
+}
---
+++ Projekt Květen 2O23/Program.cs
@@ -0,0 +1,37 @@
+﻿using Projekt_Květen_2O23;
+Inventory newInventory = new Inventory();
+bool exit = false;
+while(!exit)
+{
+    Console.WriteLine("--------------------");
+    Console.WriteLine("Vyber si akci");
+    Console.WriteLine("--------------------");
+    Console.WriteLine("1) Přidat produkt");
+    Console.WriteLine("2) Odebrat produkt");
+    Console.WriteLine("3) Zobratit Inventář");
+    Console.WriteLine("4) Ukončit");
+    Console.WriteLine("--------------------");
+    string vyberMenu = Console.ReadLine();
+    switch (vyberMenu)
+    {
+        case "1":
+            Console.Clear();
+            newInventory.AddProduct();
+            break;
+        case "2":
+            Console.Clear();
+            newInventory.RemoveProduct();
+            break;
+        case "3":
+            Console.Clear();
+            newInventory.ShowInventory();
+            break;
+        case "4":
+            exit = true;
+            break;
+        default:
+            Console.Clear();
+            Console.WriteLine("Chyba při výběru");
+            break;
+    }
+}
